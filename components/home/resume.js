e0772b60960fc87be25df053b87d2f983a7c58f6
jakovid <jacobk.dowdle@gmail.com>
2023-10-31 23:48:34 -0400

    created skeleton of resume

---
+++ components/home/resume.js
@@ -0,0 +1,11 @@
+export default function BuildResume(){
+    return(
+        <div className="flex flex-col justify-center items-center ">
+            <div className="h-2 w-full bg-purple-400 my-8"></div>
+            <div className="text-4xl font-bold">Tech Experience</div>
+            <div>
+                <div>Formation</div>
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import BuildHeader from "@/components/header"
 import BuildHomeHero from "@/components/home/hero"
+import BuildResume from "@/components/home/resume"
 
 export default function Home() {
   return (
     <main>
       <BuildHeader />
       <BuildHomeHero />
-      portfolio
+      <BuildResume />
+      
     </main>
   )
 }
