2c322e6438a89fe0ce18172aeb3acae249d3f12c
jakovid <jacobk.dowdle@gmail.com>
2023-11-19 22:38:35 -0500

    began to tell the story of OMD

--- components/home/projects.js
+++ components/home/projects.js
@@ -6,7 +6,7 @@ export default function BuildProjects(){
     return(
         <div className="flex flex-col justify-center items-center py-12">
             <div className="h-2 w-full bg-purple-400"/>
-            <div className="text-4xl font-bold p-8">Clients and Projects</div>
+            <div id="my-projects" className="text-4xl font-bold p-8">Clients and Projects</div>
             <div className="flex flex-col md:flex-row gap-8">
                 <Link href={'/projects/sky-education'}className="cursor-pointer">
                     <Image 
@@ -26,7 +26,7 @@ export default function BuildProjects(){
                     />
                 </Link>
 
-                <div className="cursor-pointer">
+                <Link href={'/projects/onemoredrink'} className="cursor-pointer">
                     <div className="px-1 py-1 bg-[#f2fb07] rounded-md flex items-center">
                         <div className="text-black px-2 bg-[#f2fb07] h-10 flex items-center text-xl">
                             OneMoreDrink
@@ -39,7 +39,7 @@ export default function BuildProjects(){
                         
                     </div>
                     
-                </div>
+                </Link>
             </div>
         </div>
     )
