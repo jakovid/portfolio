8b35b0838af87450e92bcaa8e2ed05f3292c2a85
jakovid <jacobk.dowdle@gmail.com>
2023-11-21 13:56:27 -0500

    minor changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/repository-name',
+  assetPrefix: '/repository-name/',
 }
 
 module.exports = nextConfig
